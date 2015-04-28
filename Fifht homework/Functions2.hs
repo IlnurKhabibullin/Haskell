e07e242176ea5e45edfe9a58d9ba83ecd66489b6
Khabibullin Ilnur <khabibullin_ilnur@inbox.ru>
2015-04-29 01:54:54 +0300

    Partially done hw
    esse left

--- Fifht homework/Functions2.hs
+++ Fifht homework/Functions2.hs
@@ -5,11 +5,17 @@ module Functions2 (
 	) where
 	
 	foldl_map :: (a -> b) -> [a] -> [b]  
-	foldl_map function list = foldl (\acc x -> function x : acc) [] list
+	foldl_map f list = foldl (\acc x -> f x : acc) [] list
 	
 	foldr_map :: (a -> b) -> [a] -> [b]  
-	foldr_map function list = foldr (\x acc -> function x : acc) [] list
+	foldr_map f list = foldr (\x acc -> f x : acc) [] list
 	
 	r_concatMap :: (a -> [b]) -> [a] -> [b]
 	r_concatMap f [] = []
-	r_concatMap function (a:as) = function a ++ r_concatMap function as
+	r_concatMap f (a:as) = f a ++ r_concatMap f as
+	
+	foldl_concatMap :: (a -> [b]) -> [a] -> [b]
+	foldl_concatMap f list = foldl (\acc x -> f x ++ acc) [] list
+	
+	foldr_concatMap :: (a -> [b]) -> [a] -> [b]
+	foldr_concatMap f list = foldr (\x acc -> f x ++ acc) [] list
--- Fourth homework/Trees.hs
+++ Fourth homework/Trees.hs
@@ -1,33 +1,36 @@
 module Trees where
 	
-	data BST = Leaf | NodeKey Int (Int) (BST) (BST) deriving (Show, Read, Eq)
+	--binary search tree with values in node, leafs is empty
+	data BST = BstLeaf | NodeKey Int (Int) (BST) (BST) deriving (Show, Read, Eq)
 		
 	bstInsert :: Int -> Int -> BST -> BST
-	bstInsert key value Leaf = NodeKey key value Leaf Leaf 
+	bstInsert key value BstLeaf = NodeKey key value BstLeaf BstLeaf 
 	bstInsert key value (NodeKey a b left right)   
 		| key == a = NodeKey key value left right  
 		| key < a  = NodeKey a b (bstInsert key value left) right  
 		| key > a  = NodeKey a b left (bstInsert key value right)
 	
 	--height of the BST
+	--I dont count leafs in height, so if i should, then just put 1 instead of 0
 	bstHeight :: BST -> Int
-	bstHeight Leaf = 0
+	bstHeight BstLeaf = 0
 	bstHeight (NodeKey _ _ left right) = 1 + max (bstHeight left) (bstHeight right)
 	
 	--sum of all elements in the BST
 	bstSum :: BST -> Int
-	bstSum Leaf = 0
+	bstSum BstLeaf = 0
 	bstSum (NodeKey _ value left right) = value + bstSum left + bstSum right
 	
 	--search of element with given key in the BST. I dont know what to return if there is no
 	--such key in the tree, so i desided to return an error :) 
 	bstSearch :: Int -> BST -> Int  
-	bstSearch _ Leaf = error "Not found"  
+	bstSearch _ BstLeaf = error "Not found"  
 	bstSearch key (NodeKey a b left right)  
 		| key == a = b  
 		| key < a  = bstSearch key left  
 		| key > a  = bstSearch key right
-
+	
+	--list with alternating types of values
 	data StrangeList a b = SlNull | SlValue a (StrangeList b a) deriving (Show, Read, Eq)
 	
 	--length of list
@@ -41,3 +44,15 @@ module Trees where
 	dmap sls fa fb = case sls of 
 		SlNull -> SlNull
 		SlValue x xs -> SlValue (fa x) (dmap xs fb fa)
+	
+	--  binary search trees with values only in the leafs
+	data LBST a = LbstLeaf a | LbstNodeKey Int (LBST a) (LBST a) deriving (Show, Read, Eq)
+		
+	--i dont count leafs here too cause they have no keys, only values
+	lbstHeight :: LBST a -> Int
+	lbstHeight (LbstLeaf _) = 0
+	lbstHeight (LbstNodeKey _ left right) = 1 + max (lbstHeight left) (lbstHeight right)
+	
+	tmap :: (a -> b) -> LBST a -> LBST b
+	tmap f (LbstLeaf a) = LbstLeaf (f a)
+	tmap f (LbstNodeKey n left right) = LbstNodeKey n (tmap f left) (tmap f right)
